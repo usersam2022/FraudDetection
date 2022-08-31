53fb0f646bf297ac1c7a1eb05a0740da6232aefe
usersam2022 <changei814@gmail.com>
2022-08-31 12:24:05 +0530

    added 3 new ml model code

---
+++ ml_model.py
@@ -0,0 +1,11 @@
+
+print('adding all ML related code here')
+
+def model1():
+    print('adding code for ML Model1')
+
+def model2():
+    print('adding code for ML Model2')
+
+def model3():
+    print('adding code for ML Model3')
